3d7d9df55191c7a123390b0a640c1eaf37b4684a
Dickson Boateng <dicksonboateng@proton.me>
2026-08-17 13:26:49 +0000

    fix: correct typo in CORS allowed origin (capstonne → capstone)

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(morgan('dev'))
 app.use(cors ({
     origin: [
       'http://localhost:4000',
-      'https://capstone-frontend-rust.vercel.app' 
+      'https://capstonne.vercel.app' 
     ], 
     credentials: true
 }))
